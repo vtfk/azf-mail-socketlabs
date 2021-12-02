23aef3a345c909b0f27afcb8ba4dfac74164c704
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-12-02 20:59:48 +0100

    Return result instead of using `context.res`

--- SendMail/index.js
+++ SendMail/index.js
@@ -1,7 +1,5 @@
 const handleMail = require('../lib/handle-mail');
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    context.res = await handleMail(context, req.body);
+    return await handleMail(context, req.body);
 }
--- template/index.js
+++ template/index.js
@@ -1,42 +1,40 @@
 const barhandles = require('barhandles');
-const fs = require('fs');
+const { readdirSync } = require('fs');
 const path = require('path');
 
 const dirPath = `${__dirname}/../lib/templates`;
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    
-    let templates = [];
+    const { template } = req.params
+    const templates = [];
 
-    if (!req.params.template) {
-        context.res = {
+    if (!template) {
+        return {
             status: 400,
             body: "Template name missing in the url"
-        };
-        return;
+        }
     }
 
-    fs.readdirSync(dirPath).map(fileName => {
-        let template = require(`${dirPath}/${fileName}`)();
+    readdirSync(dirPath).forEach(fileName => {
+        const templateBody = require(`${dirPath}/${fileName}`)();
         let fileNameWithoutExtension = fileName.replace(path.extname(fileName), '');
 
-        if ((req.params.template.toLowerCase() === 'all') || (req.params.template.toLowerCase() === fileNameWithoutExtension.toLowerCase())) {
+        if ((template.toLowerCase() === 'all') || (template.toLowerCase() === fileNameWithoutExtension.toLowerCase())) {
             templates.push({
                 name: fileNameWithoutExtension,
-                schema: barhandles.extractSchema(template)
+                schema: barhandles.extractSchema(templateBody)
             });
         }
     });
 
     if (templates.length > 0) {
-        context.res = {
+        return {
             status: 200,
-            body: (templates.length == 1 ? templates[0] : templates)
+            body: (templates.length === 1 ? templates[0] : templates)
         };
     }
     else {
-        context.res = {
+        return {
             status: 404,
             body: "No templates found..."
         };
